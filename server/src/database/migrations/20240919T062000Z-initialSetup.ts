f36921571a479088ced349cd0551d4cce57fd081
Vilius <vilius@vilius.net>
2024-09-19 09:44:31 +0300

    chore: add initial Migration

---
+++ server/src/database/migrations/20240919T062000Z-initialSetup.ts
@@ -0,0 +1,33 @@
+import { type Kysely, sql } from 'kysely'
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createTable('todo')
+    .addColumn('id', 'integer', (c) =>
+      c.primaryKey().generatedAlwaysAsIdentity()
+    )
+    .addColumn('title', 'varchar', (c) => c.notNull())
+    .addColumn('completed', 'boolean', (c) => c.defaultTo(false).notNull())
+    .addColumn('created_at', 'timestamptz', (c) =>
+      c.defaultTo(sql`CURRENT_TIMESTAMP`).notNull()
+    )
+    .execute()
+
+  await db.schema
+    .createTable('user')
+    .addColumn('id', 'integer', (c) =>
+      c.primaryKey().generatedAlwaysAsIdentity()
+    )
+    .addColumn('github_id', 'varchar', (c) => c.notNull().unique())
+    .addColumn('email', 'varchar', (c) => c.notNull().unique())
+    .addColumn('image', 'varchar', (c) => c.notNull())
+    .addColumn('created_at', 'timestamptz', (c) =>
+      c.defaultTo(sql`CURRENT_TIMESTAMP`).notNull()
+    )
+    .execute()
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.dropTable('todo').execute()
+  await db.schema.dropTable('user').execute()
+}
