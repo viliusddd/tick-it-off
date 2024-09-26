b7f5ce6bad09615837abbed20aa693b9564da908
Vilius <vilius@vilius.net>
2024-09-26 14:34:07 +0300

    chore: move todo.findAll input validation schema to entities

--- server/src/controllers/completion/findAll.ts
+++ server/src/controllers/completion/findAll.ts
@@ -1,25 +1,14 @@
+import { completionPaginationSchema } from '@server/entities/completion'
 import { completionRepository } from '@server/repositories/completionRepository'
 import { publicProcedure } from '@server/trpc'
 import provideRepos from '@server/trpc/provideRepos'
-import { z } from 'zod'
-// import {} from '@server/repositories/'
-
-const POSTGRES_INT_MAX = 2147483647
 
 export default publicProcedure
   .use(provideRepos({ completionRepository }))
-  .input(
-    z.object({
-      date: z.string().default(new Date().toLocaleDateString('lt')),
-      offset: z.number().int().min(0).max(POSTGRES_INT_MAX).default(0),
-      limit: z.number().int().min(1).max(100).default(20),
-    })
-  )
-  .query(async ({ input: { date, limit, offset }, ctx }) => {
-    console.log('foo')
-    const completions = await ctx.repos.completionRepository.findAll(date, {
+  .input(completionPaginationSchema)
+  .query(async ({ input: { date, limit, offset }, ctx }) =>
+    ctx.repos.completionRepository.findAll(date, {
       offset,
       limit,
     })
-    return completions
-  })
+  )
