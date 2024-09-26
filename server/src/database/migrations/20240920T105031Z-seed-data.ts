5d4186ca1dc1aac9ba8ae95904bfb57689b2f03a
Vilius <vilius@vilius.net>
2024-09-26 14:26:00 +0300

    feat: add completion table seed data

--- server/src/database/migrations/20240920T105031Z-seed-data.ts
+++ server/src/database/migrations/20240920T105031Z-seed-data.ts
@@ -23,10 +23,30 @@ const todoTitles = [
   { title: 'Try a new recipe' },
 ]
 
+const todayDate = new Date()
+const yesterdayDate = new Date(new Date().setDate(todayDate.getDate() - 1))
+
+const completionEntries = [
+  { todo_id: 1, date: todayDate },
+  { todo_id: 2, date: todayDate },
+  { todo_id: 3, date: todayDate },
+  { todo_id: 4, date: todayDate },
+  { todo_id: 17, date: todayDate },
+  { todo_id: 19, date: todayDate },
+  { todo_id: 20, date: todayDate },
+  { todo_id: 3, date: yesterdayDate },
+  { todo_id: 17, date: yesterdayDate },
+  { todo_id: 19, date: yesterdayDate },
+  { todo_id: 1, date: '2024-09-09' },
+  { todo_id: 2, date: '2024-09-24' },
+]
+
 export async function up(db: Kysely<any>) {
   await db.insertInto('todo').values(todoTitles).execute()
+  await db.insertInto('completion').values(completionEntries).execute()
 }
 
 export async function down(db: Kysely<any>) {
   db.deleteFrom('todo').execute()
+  db.deleteFrom('completion').execute()
 }
