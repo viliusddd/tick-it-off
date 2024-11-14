2566140098b3543e4a3d3ddf00961c53f562d4e1
Vilius <vilius@vilius.net>
2024-11-14 12:16:06 +0200

    chore(server): remove unused controllers

--- server/src/controllers/friend/index.ts
+++ server/src/controllers/friend/index.ts
@@ -1,14 +1,8 @@
 import { router } from '@server/trpc'
 import create from './create'
-import deleteById from './deleteById'
 import findAll from './findAll'
-import update from './update'
-import share from './share'
 
 export default router({
   create,
-  deleteById,
   findAll,
-  share,
-  update,
 })
