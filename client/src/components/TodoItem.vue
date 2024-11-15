865f0d5bde1128950933281f95d2a2fe671cb8e1
Vilius <vilius@vilius.net>
2024-11-15 14:12:04 +0200

    refactor(client): remove dark mode from todo component

--- client/src/components/TodoItem.vue
+++ client/src/components/TodoItem.vue
@@ -1,28 +1,20 @@
 <template>
   <li
-    :class="[
-      'flex items-center space-x-3 rounded-md p-3 transition duration-300 ease-in-out',
-      isDarkMode ? 'bg-gray-700 hover:bg-gray-600' : 'bg-gray-50 hover:bg-gray-100',
-    ]"
+    class="flex items-center space-x-3 rounded-md bg-gray-50 p-3 transition duration-300 ease-in-out hover:bg-gray-100"
   >
     <input
       type="checkbox"
       :checked="todo.isCompleted"
       @change="$emit('toggle', todo.id)"
-      :class="[
-        'h-5 w-5 rounded focus:ring-2 focus:ring-offset-2',
-        isDarkMode ? 'text-blue-500 focus:ring-blue-600' : 'text-blue-600 focus:ring-blue-500',
-      ]"
+      class="h-5 w-5 rounded text-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
     />
     <span
       :class="[
         'flex-grow',
         {
           'line-through': todo.isCompleted,
-          'text-gray-400': todo.isCompleted && isDarkMode,
-          'text-gray-500': todo.isCompleted && !isDarkMode,
-          'text-white': !todo.isCompleted && isDarkMode,
-          'text-gray-800': !todo.isCompleted && !isDarkMode,
+          'text-gray-400': todo.isCompleted,
+          'text-gray-800': !todo.isCompleted,
         },
       ]"
     >
@@ -30,12 +22,7 @@
     </span>
     <button
       @click="$emit('delete', todo.id)"
-      :class="[
-        'focus:outline-none focus:ring-2 focus:ring-offset-2',
-        isDarkMode
-          ? 'text-red-400 hover:text-red-300 focus:ring-red-500'
-          : 'text-red-500 hover:text-red-600 focus:ring-red-500',
-      ]"
+      class="text-red-500 hover:text-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
     >
       <TrashIcon class="h-5 w-5" />
     </button>
@@ -51,7 +38,6 @@ defineProps<{
     title: string
     isCompleted?: boolean
   }
-  isDarkMode: boolean
 }>()
 
 defineEmits<{
