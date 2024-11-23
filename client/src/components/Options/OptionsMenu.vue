9d7f130d5c287a7c9c4311e1c1e623a5a11bb003
Vilius <vilius@vilius.net>
2024-11-23 11:11:35 +0200

    feat(client): use vue Suspense to load async component

--- client/src/components/Options/OptionsMenu.vue
+++ client/src/components/Options/OptionsMenu.vue
@@ -1,20 +1,14 @@
 <template>
   <VueMenu :model="items" class="w-full md:w-60" ref="menu" id="overlay_menu" :popup="true">
     <template #start>
-      <button
-        v-ripple
-        class="hover:bg-surface-100 dark:hover:bg-surface-800 relative flex w-full cursor-pointer items-start overflow-hidden rounded-none border-0 bg-transparent p-2 pl-4 transition-colors duration-200"
-      >
-        <Avatar
-          image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
-          class="mr-2"
-          shape="circle"
-        />
-        <span class="inline-flex flex-col items-start">
-          <span class="font-bold">Foo Bar</span>
-          <span class="text-sm">email</span>
-        </span>
-      </button>
+      <Suspense>
+        <template #default>
+          <OptionsUser class="my-2" />
+        </template>
+        <template #fallback>
+          <div>Loading...</div>
+        </template>
+      </Suspense>
     </template>
     <template #item="{ item, props }">
       <a v-ripple class="flex items-center" v-bind="props.action">
@@ -32,8 +26,7 @@ import { useDark, useToggle } from '@vueuse/core'
 import { logout } from '@/stores/user'
 import { useRouter } from 'vue-router'
 import { menu } from '@/stores/user'
-import { findUserById } from '@/stores/user'
-// console.log(await findUserById({ id: 2 }))
+import OptionsUser from './OptionsUser.vue'
 
 const router = useRouter()
 
