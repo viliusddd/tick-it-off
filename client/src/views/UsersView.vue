b6060b090280c5ad71ea82f0308489f279b76533
Vilius <vilius@vilius.net>
2024-11-23 12:59:59 +0200

    feat(client): add list of users to add

--- client/src/views/UsersView.vue
+++ client/src/views/UsersView.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
-    <Listbox v-model="selectedUser" :options="users" filter optionLabel="firstName" />
+    <Listbox v-model="selectedUser" :options="users" optionLabel="firstName" :filter="true">
+      <template #option="slotProps">
+        <div class="flex items-center justify-end gap-2">
+          <div>{{ slotProps.option.firstName }} {{ slotProps.option.lastName }}</div>
+          <div>[rel type badge]</div>
+          <Button
+            icon="pi pi-plus"
+            class="p-button-rounded p-button-text"
+            @click.stop="changeStatus(slotProps.option.id)"
+          />
+        </div>
+      </template>
+    </Listbox>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, type Ref, ref } from 'vue'
+import { onMounted, ref, type Ref } from 'vue'
 import { trpc } from '@/trpc'
 import type { UserPublic } from '@server/shared/types'
+import Listbox from 'primevue/listbox'
+import Button from 'primevue/button'
+
 const users: Ref<UserPublic[]> = ref([])
 const selectedUser: Ref<UserPublic | null> = ref(null)
 
 onMounted(async () => {
   users.value = await trpc.user.findAll.query({ offset: 0, limit: 10 })
 })
+
+const changeStatus = async (user: UserPublic) => {
+  console.log('Adding user:', user)
+}
 </script>
