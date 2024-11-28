721c123f0274a01c3c31d0c2c32156526cc9ac73
Vilius <vilius@vilius.net>
2024-11-28 14:50:19 +0200

    chore(client): adjust UsersView to show users properly and with feiend tag

--- client/src/views/UsersView.vue
+++ client/src/views/UsersView.vue
@@ -1,7 +1,6 @@
 <template>
   <Listbox
-    v-model="selectedUser"
-    :options="users.filter((usr) => usr.id != userStore.authUserId)"
+    :options="usersUpdated"
     optionLabel="firstName"
     :filter="true"
     :pt="{
@@ -14,19 +13,23 @@
   >
     <template #option="slotProps">
       <div class="flex items-center gap-2">
-        <div>{{ slotProps.option.firstName }} {{ slotProps.option.lastName }}</div>
-        <Tag value="foo" />
+        <div>{{ slotProps.option.userbFirstName }} {{ slotProps.option.userbLastName }}</div>
+        <div v-if="slotProps.option.isFriend">
+          <Tag value="friend" severity="info" />
+        </div>
       </div>
       <div>
         <VueButton
+          v-if="!slotProps.option.isFriend"
           icon="pi pi-plus"
           class="p-button-rounded p-button-text"
-          @click.stop="changeStatus(slotProps.option.id)"
+          @click.stop="changeStatus(slotProps.option.userbId)"
         />
         <VueButton
-          icon="pi pi-ban"
+          v-else
+          icon="pi pi-minus"
           class="p-button-rounded p-button-text"
-          @click.stop="banUser(slotProps.option.id)"
+          @click.stop="banUser(slotProps.option.userbId)"
         />
       </div>
     </template>
@@ -34,25 +37,58 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, type Ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { trpc } from '@/trpc'
-import type { UserPublic } from '@server/shared/types'
 import { useUserStore } from '@/stores/userStore'
 import { Tag, Listbox } from 'primevue'
 
-const selectedUser: Ref<UserPublic | null> = ref(null)
-const users = ref<UserPublic[]>([])
-
 const userStore = useUserStore()
 
-onMounted(async () => {
-  users.value = await trpc.user.findAll.query()
+type UsersConnections = {
+  useraId: number | null
+  userbId: number | null
+  useraFirstName: string | null
+  useraLastName: string | null
+  userbFirstName: string | null
+  userbLastName: string | null
+}
+
+type UserUpdated = {
+  userbId: number | null
+  userbFirstName: string | null
+  userbLastName: string | null
+  isFriend: boolean
+}
+
+const users = ref<UsersConnections[]>([])
+
+const usersUpdated = computed(() => {
+  return users.value.reduce((usersAccum: UserUpdated[], usr) => {
+    const { userbId, userbFirstName, userbLastName } = usr
+
+    // remove self from list
+    if (usr.userbId === userStore.authUserId) return usersAccum
+
+    let isFriend = false
+    if (usr.useraId === userStore.authUserId) isFriend = true
+
+    // get index of entry in arr, to eliminate duplicates
+    const index = usersAccum.findIndex((accUsr) => accUsr.userbId === userbId)
+
+    if (index === -1) usersAccum.push({ userbId, userbFirstName, userbLastName, isFriend })
+
+    return usersAccum
+  }, [])
 })
 
-const changeStatus = async (user: UserPublic) => {
-  console.log('Adding user:', user)
+onMounted(async () => {
+  users.value = await trpc.userRelationship.findAllWithUsers.query()
+})
+
+const changeStatus = async (userId: number) => {
+  console.log('Adding user:', userId)
 }
-const banUser = async (user: UserPublic) => {
-  console.log('Ban user:', user)
+const banUser = async (userId: number) => {
+  console.log('Ban user:', userId)
 }
 </script>
