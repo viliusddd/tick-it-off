3df5d790615cb9c8fb6b111fb04789c6d401e906
Vilius <vilius@vilius.net>
2024-12-08 08:04:18 +0200

    chore(client): change to material theme, remove v-calendar

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,9 @@
 import './assets/style.css'
-import 'v-calendar/style.css'
 import {createApp} from 'vue'
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
 import 'primeicons/primeicons.css'
-import Aura from '@primevue/themes/aura'
 import {createPinia} from 'pinia'
-import {setupCalendar} from 'v-calendar'
 
 import Checkbox from 'primevue/checkbox'
 import Toast from 'primevue/toast'
@@ -17,6 +14,7 @@ import Avatar from 'primevue/avatar'
 import Ripple from 'primevue/ripple'
 import Badge from 'primevue/badge'
 import OverlayBadge from 'primevue/overlaybadge'
+import Material from '@primevue/themes/material'
 
 import App from './App.vue'
 import router from './router'
@@ -25,17 +23,9 @@ const app = createApp(App)
 app
   .use(createPinia())
   .use(router)
-  .use(setupCalendar, {})
   .use(PrimeVue, {
     ripple: true,
-    theme: {
-      preset: Aura,
-      options: {
-        prefix: 'p',
-        darkModeSelector: 'system',
-        cssLayer: false
-      }
-    }
+    theme: {preset: Material}
   })
   .use(ToastService)
 
