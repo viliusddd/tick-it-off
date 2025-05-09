7285669dc73e191e05d2dfed9588ceb2a934d987
Vilius <vilius@vilius.net>
2025-05-09 12:57:05 +0300

    feature: cancel delete confirm when pressed outside

--- client/src/components/Todo/ConfirmDeleteButton.vue
+++ client/src/components/Todo/ConfirmDeleteButton.vue
@@ -1,21 +1,23 @@
 <template>
-  <template v-if="!showConfirm">
-    <button @click="showConfirm = true" :class="trashClass" :style="iconStyle">
-      <TrashIcon :class="iconSizeClass" />
-    </button>
-  </template>
-  <template v-else>
-    <button @click="showConfirm = false" :class="cancelClass" :style="iconStyle">
-      <XMarkIcon :class="iconSizeClass" />
-    </button>
-    <button @click="onConfirm" :class="confirmClass" :style="iconStyle">
-      <CheckIcon :class="iconSizeClass" />
-    </button>
-  </template>
+  <span ref="rootEl">
+    <template v-if="!showConfirm">
+      <button @click="showConfirm = true" :class="trashClass" :style="iconStyle">
+        <TrashIcon :class="iconSizeClass" />
+      </button>
+    </template>
+    <template v-else>
+      <button @click="showConfirm = false" :class="cancelClass" :style="iconStyle">
+        <XMarkIcon :class="iconSizeClass" />
+      </button>
+      <button @click="onConfirm" :class="confirmClass" :style="iconStyle">
+        <CheckIcon :class="iconSizeClass" />
+      </button>
+    </template>
+  </span>
 </template>
 
 <script setup lang="ts">
-import {ref, computed} from 'vue'
+import {ref, computed, onMounted, onUnmounted} from 'vue'
 import {TrashIcon, CheckIcon, XMarkIcon} from '@heroicons/vue/24/solid'
 
 const props = defineProps({
@@ -48,8 +50,24 @@ const trashClass = computed(
 const cancelClass = computed(() => `${props.cancelColor} mr-1 focus:outline-none`)
 const iconStyle = ''
 
+const rootEl = ref<HTMLElement | null>(null)
+
 function onConfirm() {
   emit('confirm')
   showConfirm.value = false
 }
+
+function handleClickOutside(event: MouseEvent) {
+  if (!showConfirm.value) return
+  if (rootEl.value && !rootEl.value.contains(event.target as Node)) {
+    showConfirm.value = false
+  }
+}
+
+onMounted(() => {
+  document.addEventListener('mousedown', handleClickOutside)
+})
+onUnmounted(() => {
+  document.removeEventListener('mousedown', handleClickOutside)
+})
 </script>
